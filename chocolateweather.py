4d68aa79ea30e20dbfdeb1f17254b984dadf79b5
Cristina Rodriguez <yosola@gmail.com>
2016-07-12 20:39:00 -0700

    Add a little more of sarcarsm

--- chocolateweather.py
+++ chocolateweather.py
@@ -10,10 +10,8 @@ def get_weather(place):
 
 
 def if_clouds(place):
-
     weather = get_weather(place) 
     clouds = weather.get_clouds()
-
     if clouds >=5:
         karl = weather.get_detailed_status()
         return karl
@@ -21,10 +19,10 @@ def if_clouds(place):
 def forecast_sun(place):
     forecast = owm.daily_forecast(place)
     sun = forecast.will_have_sun()
-    if sun == True:
-        return "This is a miracle. You will see the sun! Let's Eat chocolate to celebrate"
-    else:
+    if sun == False:
         return "What do you expect from San Francisco?. Let's go for some chocolate"
+    else:
+        return "This is a miracle. You will see the sun! Let's Eat chocolate to celebrate" 
 
 def main():
 
@@ -34,9 +32,11 @@ def main():
     place = "San Francisco, CA"
     if weather_city == "Y":
         w = get_weather(place)
-        print w.get_temperature("celsius")
+        celsius = w.get_temperature("celsius")
+        print celsius
+        print ("Wow! Apparently the odds are greater that you would have %s for today" %(celsius["temp"]))
         print forecast_sun(place)
-        prompt1 = ("""Let's check if you need that chocolate. Press Enter when you're ready """)
+        prompt1 = ("Neverless, let's check one more time if you need that chocolate. Press Enter when you're ready """)
         user_choice1 = raw_input(prompt1)
         if user_choice1 == "":
             lucky = if_clouds(place)
